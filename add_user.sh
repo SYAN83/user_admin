29614e33d6020c7b7f14dfdb5d6ae2847c5744d5
Shu Yan <yanshu.usc@gmail.com>
2017-08-07 07:58:06 +0000

    add HDFS

--- add_user.sh
+++ add_user.sh
@@ -8,28 +8,42 @@ if [ "$#" -ne 1 ]
 then
 	usage
 fi
-
+# create log file if not existed
 LOGFILE=users.log
 touch $LOGFILE
 chmod 600 $LOGFILE
-
+LOGINS=credentials.yaml
+touch $LOGFILE
+chmod 600 $LOGFILE
+# create username and password
 USER=$1
-TIME=$(date)
 PSWD=$(openssl rand -base64 8 | tr '[:punct:]' $((RANDOM % 10)) | tr 1IloO0 2iL789)
 PSWDcrypt=$(openssl passwd -crypt $PSWD)
-
+# create Linux user
 echo "Creating user $USER..."
 sudo useradd -m -p $PSWDcrypt $USER
 if [ $? -ne 0 ]; then
-	log="[$TIME] FAILED TO CREATE USER $USER"
+	log="[$(date)] FAILED TO CREATE USER $USER"
 	echo "Failed to create user $USER."
 else
-	log="[$TIME] CREATED USER $USER $PSWD"
+	log="[$(date)] CREATED USER $USER"
+	echo "$USER: $PSWD" >> $LOGINS
 	echo "User $USER has been sucessfully created."
 fi
-
-cat >> $LOGFILE <<EOF
-$log
-EOF
-
+# create HDFS directory
+hadoop fs -mkdir /user/$USER
+if [ $? -ne 0 ]; then
+        log="$log\n[$(date)] FAILED TO CREATE HDFS $USER"
+        echo "Failed to create HDFS directory for $USER."
+else
+        hadoop fs -chown -R $USER:$USER /user/$USER
+	if [ $? -ne 0 ]; then
+		log="$log\n[$(date)] FAILED TO CHANGE HDFS PERMISSION $USER"
+		echo "Failed to change HDFS permission for $USER."
+	else
+		log="$log\n[$(date)] CREATED HDFS $USER"
+		echo "HDFS dir for $USER has been sucessfully created."
+	fi
+fi
+echo -e $log >> $LOGFILE
 
--- del_user.sh
+++ del_user.sh
@@ -9,21 +9,28 @@ then
 	usage
 fi
 
+LOGFILE=users.log
 USER=$1
 TIME=$(date)
 
 echo "Deleting user $USER..."
+# delete HDFS directory
+hadoop fs -rm -r /user/$USER
+if [ $? -ne 0 ]; then
+        log="[$TIME] FAILED TO DELETE HDFS $USER"
+        echo "Failed to delete HDFS dir for $USER."
+else
+        log="[$TIME] DELETED HDFS $USER"
+        echo "HDFS dir for $USER has been sucessfully deleted."
+fi
+# delete Linux user
 sudo userdel -r $USER
 if [ $? -ne 0 ]; then
-	log="[$TIME] FAILED TO DELETE USER $USER"
+	log="$log\n[$TIME] FAILED TO DELETE USER $USER"
 	echo "Failed to delete user $USER."
 else
-	log="[$TIME] DELETED USER $USER."
+	log="$log\n[$TIME] DELETED USER $USER"
 	echo "User $USER has been sucessfully deleted."
 fi
-
-cat >> users.log <<EOF
-$log
-EOF
-
+echo -e $log >> $LOGFILE 
 
