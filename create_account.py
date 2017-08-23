dceca9783ae2c8a56e5c16023fe9f374e6802e69
Shu Yan <yanshu.usc@gmail.com>
2017-08-23 14:37:11 +0000

    add del in create_account.py

--- create_account.py
+++ create_account.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import subprocess
 
-for i in range(20):
-    subprocess.call('./add_user.sh -u tempuser_{} -g student'.format(i), shell=True)
+for i in range(22):
+    # subprocess.call('./add_user.sh -u tempuser_{} -g student'.format(i), shell=True)
+    subprocess.call('./del_user.sh tempuser_{}'.format(i), shell=True)
