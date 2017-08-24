62304717c103f76c13fa1f9c775390ec465bce8b
Shu Yan <yanshu.usc@gmail.com>
2017-08-24 19:35:23 +0000

    create account

--- create_account.py
+++ create_account.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import subprocess
 
-for i in range(22):
+with open('students.txt') as f:
+    for line in f:
+        student = line.strip().split(':',1)[0]
+        subprocess.call('./add_user.sh -u {} -g student -e 30'.format(student), shell=True)
+    print('done')
+
+# for i in range(22):
     # subprocess.call('./add_user.sh -u tempuser_{} -g student'.format(i), shell=True)
-    subprocess.call('./del_user.sh tempuser_{}'.format(i), shell=True)
+    # subprocess.call('./del_user.sh tempuser_{}'.format(i), shell=True)
